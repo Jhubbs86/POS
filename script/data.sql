1febefe7da7f4d3435099b7ada6be7569c72ae51
changzheng115 <changzheng115@163.com>
2015-01-15 15:46:33 +0800

    一级菜单数据更新

--- script/data.sql
+++ script/data.sql
@@ -104,6 +104,904 @@ BEGIN
 			)
 END
 
+--add by zcz
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '组织' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           (
+				'组织'
+				,''
+				,0
+				,4
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '统战' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'统战'
+				,''
+				,0
+				,5
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '工会' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'工会'
+				,''
+				,0
+				,6
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '妇联' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'妇联'
+				,''
+				,0
+				,7
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '民政社救' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'民政社救'
+				,''
+				,0
+				,8
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '老龄' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'老龄'
+				,''
+				,0
+				,9
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '护塘小区管理' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'护塘小区管理'
+				,''
+				,0
+				,10
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '老年活动室' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'老年活动室'
+				,''
+				,0
+				,11
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '门卫' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'门卫'
+				,''
+				,0
+				,12
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '丧事简办' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'丧事简办'
+				,''
+				,0
+				,13
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '各类证明' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'各类证明'
+				,''
+				,0
+				,14
+				,1
+				,0
+			)
+END
+
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '纪检' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'纪检'
+				,''
+				,0
+				,15
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '调解' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'调解'
+				,''
+				,0
+				,16
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '安全' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'安全'
+				,''
+				,0
+				,17
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '农副业' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'农副业'
+				,''
+				,0
+				,18
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '河道保洁' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'河道保洁'
+				,''
+				,0
+				,19
+				,1
+				,0
+			)
+END
+
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '市场管理' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'市场管理'
+				,''
+				,0
+				,20
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '水电管理' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'水电管理'
+				,''
+				,0
+				,21
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '综治群众工作室' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'综治群众工作室'
+				,''
+				,0
+				,22
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '宣传' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'宣传'
+				,''
+				,0
+				,23
+				,1
+				,0
+			)
+END
+
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '文教卫生' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'文教卫生'
+				,''
+				,0
+				,24
+				,1
+				,0
+			)
+END
+
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '红十字' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'红十字'
+				,''
+				,0
+				,25
+				,1
+				,0
+			)
+END
+
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '经济管理' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'经济管理'
+				,''
+				,0
+				,26
+				,1
+				,0
+			)
+END
+
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '出纳' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'出纳'
+				,''
+				,0
+				,27
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '卫生室' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'卫生室'
+				,''
+				,0
+				,28
+				,1
+				,0
+			)
+END
+
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '土地房屋' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'土地房屋'
+				,''
+				,0
+				,29
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '治保' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'治保'
+				,''
+				,0
+				,30
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '民兵' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'民兵'
+				,''
+				,0
+				,31
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '家庭财产保险管理' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'家庭财产保险管理'
+				,''
+				,0
+				,32
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '团支部' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'团支部'
+				,''
+				,0
+				,33
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '电子政务平台管理' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'电子政务平台管理'
+				,''
+				,0
+				,34
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '会议记录' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'会议记录'
+				,''
+				,0
+				,35
+				,1
+				,0
+			)
+END
+
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '会务接待' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'会务接待'
+				,''
+				,0
+				,36
+				,1
+				,0
+			)
+END
+
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '会场布置' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'会场布置'
+				,''
+				,0
+				,37
+				,1
+				,0
+			)
+END
+
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '电教设施管理' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'电教设施管理'
+				,''
+				,0
+				,38
+				,1
+				,0
+			)
+END
+
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '科普活动室' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'科普活动室'
+				,''
+				,0
+				,39
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '村级招商引资' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'村级招商引资'
+				,''
+				,0
+				,40
+				,1
+				,0
+			)
+END
+
+
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '水利排管' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'水利排管'
+				,''
+				,0
+				,41
+				,1
+				,0
+			)
+END
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '农业组织化' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'农业组织化'
+				,''
+				,0
+				,42
+				,1
+				,0
+			)
+END
+
+
+IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '项目建设协调' HAVING(COUNT(*)>0))
+BEGIN
+     INSERT INTO [CWXT].[dbo].[Menu]
+           (
+				[ChineseName]
+				,[URL]
+				,[Parent]
+				,[DisplayOrder]
+				,[IsValid]
+				,[IsLeaf]
+			)
+     VALUES
+           
+				'项目建设协调'
+				,''
+				,0
+				,43
+				,1
+				,0
+			)
+END
+
+
+--end add
+
 
 IF   NOT EXISTS (SELECT COUNT(*) FROM [CWXT].[dbo].[Menu] WHERE [IsValid] = 1 AND [ChineseName] = '用户组管理' HAVING(COUNT(*)>0))
 BEGIN
